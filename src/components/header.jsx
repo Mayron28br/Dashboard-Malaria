aa469167128fe1c2ebf85778abe9fa4b8a26850c
Mayron Wilke <mayronwilke28@gmail.com>
2024-10-25 00:23:27 -0300

    start of the project

---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './header.css'
+import { Link } from 'react-router-dom'
+
+const header = () => {
+  return (
+    <header>
+        <nav>
+            <p>Home</p>
+        </nav>
+    </header>
+  )
+}
+
+export default header
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home.css'
+import Header from '../../components/header'
+
+const home = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default home
